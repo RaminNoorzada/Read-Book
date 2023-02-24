deae73091462b5f336360e81ada9b962272b240e
Your Name <you@example.com>
2023-02-23 19:16:44 -0800

    files modifide

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Read-Book
+# Tech-blog
+
+![GitHub licence](https://img.shields.io/badge/licence-MIT-gree.svg)
+
+## Deccription
+
+This is a tech-blog site. It allows developers to publish their blog posts and comment on other developers’ posts as well. The app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.
+
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+
+## installation
+
+
+Clone this repository to your local machine.
+Navigate to the root directory of the application in your terminal.
+Run npm install to install the necessary dependencies.
+Create a .env file at the root directory of the application and add the necessary environment variables. Refer to the .env.EXAMPLE file for a list of required variables.
+Set up a MySQL database and update the config.json file in the config directory with your database credentials.
+Run npm run seed to seed the database with some initial data.
+Run npm start to start the server.
+
+## Usage
+Once the server is started, navigate to http://localhost:3001 in your browser to use the application. You can create a new user account, log in, create new blog posts, and comment on existing blog posts.
+
+## Deployed app
+The app function images.(work in progress)
+
+## License
+This application is licensed under the MIT License. See the LICENSE file for more information.
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+const path = require('path'); //Import Node.js path module
+const express = require('express'); //Import Express.js lib
+const session = require('express-session'); //Import Express-session middleware
+const exphbs = require('express-handlebars'); //Import Express handlebars tems
+const helpers = require('./utils/helpers'); //Import custrm helpers modu;e
+
+const routes = require('./controllers') //Import app's controllers
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; //Setting the port number
 
-const sequelize = require('./config/config');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
+const sess = { 
   secret: 'Super secret secret',
   cookie: {
     maxAge: 300000,
@@ -25,20 +27,21 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+app.use(session(sess)); //Use the express-session middleware with config ops
 
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
+app.use(express.json()); //Parsing JSON data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
-app.listen(PORT, () => {
+
+app.listen(PORT, () => { //Start tthe server
   console.log(`App listening on port ${PORT}!`);
   sequelize.sync({ force: false });
 });
