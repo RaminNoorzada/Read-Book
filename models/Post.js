58d63c60f8cd6102193fdf68881c56265a401356
Your Name <you@example.com>
2023-03-16 00:45:02 -0700

    posts.js file added

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,42 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/config');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
 Post.init(
   {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    title: {
+      type: DataTypes.STRING(25),
+      allowNull: false,
+      unique: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      },
+    
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
   },
   {
     sequelize
