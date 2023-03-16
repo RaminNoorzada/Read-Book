312a006da4e886e1f5e2ddb79229b3cd6a2ff61d
Your Name <you@example.com>
2023-03-16 02:35:14 -0700

    index.js file added

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -6,8 +6,24 @@ const deleteFormHandler = async (event) => {
     ];
 
     console.log(id);
-    };
     
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+      body: JSON.stringify({
+        post_id: id,
+      }),
+      Headers: {
+        'Content-Type': 'aplication/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+        
+      }
+  }
     document
       .querySelector('#delete')
       .addEventListener('click', deleteFormHandler);
--- public/js/edit.js
+++ public/js/edit.js
@@ -1,34 +1,32 @@
 const editFormHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#title').value.trim();
-  const content = document.querySelector('#content').value.trim();
 
-  await fetch(`/api/post/${postId}`, {
+  const title = document.querySelector('#title').Value.trim();
+  const content = document.querySelector('#content').Value.trim();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  
+  const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
-    content: JSON.stringify({
-      title,
-      content
+    body: JSON.stringify({
+      post_id: id, title, content
     }),
-    headers: {
-      'Content-Type': 'application/json'
+    Headers: {
+      'Content-Type': 'aplication/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+      
     }
-  });
-
-  document.location.replace('/dashboard');
-};
-
-const deleteClickHandler = async function() {
-  await fetch(`/api/post/${postId}`, {
-    method: 'DELETE'
-  });
-
-  document.location.replace('/dashboard');
-};
-
-document
-  .querySelector('#edit-post-form')
-  .addEventListener('submit', editFormHandler);
-document
-  .querySelector('#delete-btn')
-  .addEventListener('click', deleteClickHandler);
+}
+  document
+    .querySelector('#update')
+    .addEventListener('click', deleteFormHandler);
