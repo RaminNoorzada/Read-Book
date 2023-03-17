74d9fdc90d4dfd5b7ea5f4e5b34ddf23ea9a56b8
Your Name <you@example.com>
2023-03-17 00:11:48 -0700

    server.js updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 });
 
 // get single post
-router.get('/post/:id',withAuth async (req, res) => {
+router.get('/post/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.findOne({
       where: {
